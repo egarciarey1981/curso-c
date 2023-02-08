7d7abb8f488a091ca56b87842234ff5b32dca682
Eliecer García Rey <egarciarey1981@gmail.com>
2023-02-08 18:52:38 +0100

    día 2

--- dia-02/funciones.c
+++ dia-02/funciones.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void titulo()
+{
+    printf("ARITMETICA\n");
+}
+
 int doble(int x)
 {
     return x * 2;
@@ -17,6 +22,7 @@ int main()
     int unNumero = 3;
     int otroNumero = 4;
 
+    titulo();
     printf("El doble de %d es %d\n", unNumero, doble(unNumero));
     printf("La suma de %d y %d es %d\n", unNumero, otroNumero, suma(unNumero, otroNumero));
 
